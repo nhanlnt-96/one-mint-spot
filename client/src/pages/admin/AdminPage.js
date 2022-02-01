ab1c89483652063545869ad591b52f6ece622f9e
nhanlnt-96 <nhanlnt@hotmail.com>
2022-02-01 23:44:25 +0700

    [homepage]: Complete create homepage component

--- client/src/pages/admin/AdminPage.js
+++ client/src/pages/admin/AdminPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import AdminHeader from "components/adminHeader/AdminHeader";
+import Logo from "assets/imgs/bgLogin.svg";
 
 import "./AdminPage.scss";
 
@@ -8,6 +9,12 @@ const AdminPage = () => {
   return (
     <Container fluid className="admin-page-container">
       <AdminHeader/>
+      <Container className="admin-page-content d-flex flex-column justify-content-center align-items-center">
+        <Row className="d-flex flex-column justify-content-center align-items-center">
+          <img src={Logo} alt="logo"/>
+          <h1 className="title">Welcome to <span>Project name</span> Admin page</h1>
+        </Row>
+      </Container>
     </Container>
   );
 };
