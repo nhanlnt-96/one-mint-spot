4fbdf974bf3c9590da463ea1b92eed85ad513a07
nhanlnt-96 <nhanlnt@hotmail.com>
2022-02-04 22:26:17 +0700

    [header]: Complete create header component

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -1,20 +1,11 @@
 import React from "react";
 import {Container} from "react-bootstrap";
-import MintBoxComp from "../../components/mintBox/MintBoxComp";
-import {Link} from "react-router-dom";
+import MainHeader from "components/mainHeader/MainHeader";
 
 const Homepage = () => {
   return (
-    <Container fluid className="bg-dark">
-      <Container className="d-flex flex-column justify-content-center align-items-center" style={{minHeight: "100vh"}}>
-        <h1 className="h1" style={{color: "#fff"}}>Welcome to Mint App template</h1>
-        <div style={{width: "60%"}}>
-          <MintBoxComp/>
-        </div>
-        <div style={{marginTop: 24}}>
-          <Link to="/admin">Admin Page</Link>
-        </div>
-      </Container>
+    <Container fluid style={{height: "100vh"}} className="bg-dark">
+      <MainHeader/>
     </Container>
   );
 };
