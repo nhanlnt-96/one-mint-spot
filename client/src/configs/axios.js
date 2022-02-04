06a56604a0fa9a8f219dae60be21d4034889bebf
nhanlnt-96 <nhanlnt@hotmail.com>
2022-02-04 19:42:55 +0700

    [banner]: Working on create banner

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: 'https://landz-admin.herokuapp.com',
   headers: { accessToken: `${localStorage.getItem('accessToken')}` },
 });
 
