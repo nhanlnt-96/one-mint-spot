63418755055f8e79a2f0c1d3c483d520ce2dfe4a
nhanlnt-96 <nhanlnt@hotmail.com>
2022-02-20 21:14:35 +0700

    [deploy]: Deployed

--- client/src/configs/portfolioData.js
+++ client/src/configs/portfolioData.js
@@ -9,37 +9,45 @@ export const portfolioData = [
   {
     siteName: "SpaceTurtles",
     siteUrl: "https://nft-landing.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: `We as the creators of the “Space Turtles NFT” aims to illustrate the idea that everything is possible. We chose the turtle, the slowest creature, and space a destination even beyond the top of the world.`,
     preview: Turtle
   },
   {
     siteName: "Babbling Baboon",
     siteUrl: "https://baboon-page.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: "The Babbling Baboons Club have been found roaming around causing havoc! We need your help to" +
+      " capture and tame these silly things before it’s too late.",
     preview: Baboon
   },
   {
     siteName: "Dope Goats",
     siteUrl: "https://dope-goats.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: "Dope Goats Club are a dope collection of 2,222 goats designed by upcoming artist" +
+      " SipTee. Not only do these cuddly goats have special abilities that can only be unlocked if you are in the club, but all holders who mint two goats will receive a free baby goat on drop day. These will be the rarest of our collection.",
     preview: Goats
   },
   {
     siteName: "Teddy Gang CLub",
     siteUrl: "https://teddy-gang.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: "The Teddy Gang Club is an outlaw group of teddy bears living in the Etheruem network. Although" +
+      " they are known for their lawless behaviors, they look after their own and often do good deeds.",
     preview: Teddy
   },
   {
     siteName: "Boobies",
     siteUrl: "https://boobies-bb767.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: "We are creating an immersive metaverse experience, complete with role-playing games where you will" +
+      " be able to use your NFT as in-game avatars, train your companion NFTS, and" +
+      " hang out with your friends.",
     preview: Boobies
   },
   {
     siteName: "Fruity Fools",
     siteUrl: "https://fruity-fools.web.app/",
-    // description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, officiis.",
+    description: "Fruity Fools collection is being introduced to the NFT community by a team of enthusiasts by" +
+      " taking our own comedic edge on Fruit becoming characterized. With the goal of" +
+      " bringing out the inner child of our community members we intend on creating art collections of Fruity" +
+      " Fools as bragging rights for our Holders.",
     preview: Fruity
   },
 ];
