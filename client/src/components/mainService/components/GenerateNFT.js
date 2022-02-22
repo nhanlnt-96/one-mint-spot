b2999857ce132580f4283767a3bd9a62fc5e6fe4
nhanlnt-96 <nhanlnt@hotmail.com>
2022-02-22 23:48:26 +0700

    [master]: Update site

--- client/src/components/mainService/components/GenerateNFT.js
+++ client/src/components/mainService/components/GenerateNFT.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {ServiceTitle} from "components/mainService/components/ServiceTitle";
 import NFT from "assets/icons/nft.png";
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 
 export const GenerateNFT = () => {
   return (
     <>
       <ServiceTitle title={"generate nft"} titleIcon={NFT}/>
       <Container className="generate-nft-container">
-        <div className="desc">Generate NFT from layers design.</div>
+        <Row className="generate-nft-content">
+          <div className="desc">Generate NFT from layers design.</div>
+        </Row>
       </Container>
     </>
   );
